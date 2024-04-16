b57c8c85d853df6ed7c8eef0333fe985bcc315c4
Emad Adel <emadadel1920@gmail.com>
2024-04-16 05:47:51 +0200

    Update rm.ps1

--- rm.ps1
+++ rm.ps1
@@ -1,31 +1,36 @@
 Write-Host "Deleting registry keys for removing standard folders..."
 
-Write-Host "Deleting 3D Objects folder..."
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}" -Force
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}" -Force
+$folders = @(
+    "{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}",
+    "{f86fa3ab-70d2-4fc7-9c99-fcbf05467f3a}",
+    "{3dfdf296-dbec-4fb4-81d1-6a3438bcf4de}",
+    "{24ad3ad4-a569-4530-98e1-ab02f9417aa8}",
+    "{088e3905-0323-4b02-9826-5d99428e115f}",
+    "{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}",
+    "{d3162b92-9365-467a-956b-92703aca08af}"
+)
 
-Write-Host "Deleting Videos folder..."
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{f86fa3ab-70d2-4fc7-9c99-fcbf05467f3a}" -Force
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{f86fa3ab-70d2-4fc7-9c99-fcbf05467f3a}" -Force
+foreach ($folder in $folders) {
+    $registryPath = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\$folder"
+    $wowRegistryPath = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\$folder"
 
-Write-Host "Deleting Music folder..."
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{3dfdf296-dbec-4fb4-81d1-6a3438bcf4de}" -Force
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{3dfdf296-dbec-4fb4-81d1-6a3438bcf4de}" -Force
+    if (Test-Path $registryPath) {
+        Remove-Item -Path $registryPath -Force
+    }else
+    {
+        Clear-Host
+        Write-Host "It has already been deleted, you good to go :)"
+    }
 
-Write-Host "Deleting Pictures folder..."
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{24ad3ad4-a569-4530-98e1-ab02f9417aa8}" -Force
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{24ad3ad4-a569-4530-98e1-ab02f9417aa8}" -Force
-
-Write-Host "Deleting Downloads folder..."
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{088e3905-0323-4b02-9826-5d99428e115f}" -Force
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{088e3905-0323-4b02-9826-5d99428e115f}" -Force
-
-Write-Host "Deleting Desktop folder..."
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}" -Force
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}" -Force
-
-Write-Host "Deleting Documents folder..."
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{d3162b92-9365-467a-956b-92703aca08af}" -Force
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{d3162b92-9365-467a-956b-92703aca08af}" -Force
+    if (Test-Path $wowRegistryPath) 
+    {
+        Remove-Item -Path $wowRegistryPath -Force
+    }
+    else
+    {
+        Clear-Host
+        Write-Host "It has already been deleted, you good to go :)"
+    }
+}
 
 Start-Process "explorer.exe"
