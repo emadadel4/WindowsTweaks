acdecd8b639a6242b94aae8beeda02b2cd1b96e9
Emad Adel <emadadel1920@gmail.com>
2024-04-16 04:37:08 +0200

    Update rm.ps1

--- rm.ps1
+++ rm.ps1
@@ -27,3 +27,5 @@ Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\W
 Write-Host "Deleting Documents folder..."
 Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{d3162b92-9365-467a-956b-92703aca08af}" -Force
 Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{d3162b92-9365-467a-956b-92703aca08af}" -Force
+
+Start-Process "explorer.exe"
