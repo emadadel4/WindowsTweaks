ecc3025f7fde9e0620fbad419587e3da109be741
Emad Adel <emadadel1920@gmail.com>
2025-02-14 07:29:18 +0200

    Update OneDrive-Uninstaller.ps1

--- OneDrive-Uninstaller.ps1
+++ OneDrive-Uninstaller.ps1
@@ -1,6 +1,3 @@
-# Ensure administrator privileges
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) { Write-Host "Restarting script with administrator privileges..." -ForegroundColor Yellow; $arguments = "-NoProfile -ExecutionPolicy Bypass -File `"{0}`" -MyArgument {1}" -f $PSCommandPath, $MyArgument; Start-Process PowerShell.exe -ArgumentList $arguments -Verb RunAs; exit }
-
 # Terminate OneDrive and initiate uninstallation
 Write-Host "Terminating OneDrive process and initiating uninstallation"
 taskkill.exe /F /IM "OneDrive.exe"
